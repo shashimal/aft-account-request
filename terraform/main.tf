bc294493a58d3c3f0c7ea537d24a3b54e06110d7
Duleendra Shashimal <duleendra@bambu.co>
2022-11-19 15:27:08 +0800

    add sandbox-aft

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "shashimal.warakagoda+sandboxaft@gmail.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "shashimal.warakagoda+sandboxaft@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
